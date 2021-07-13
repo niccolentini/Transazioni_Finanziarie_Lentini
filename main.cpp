7ad0ec5a4ae0f19998b78b50d51d4857eb39a8ce
niccolentini <87321791+niccolentini@users.noreply.github.com>
2021-07-13 16:58:59 +0200

    aggiunti alcuni metodi per operare con files

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include "string"
 #include "Bank.h"
 #include "User.h"
+#include <fstream>
 
 int Bank::bankAccountNumber = 0;
 
@@ -11,6 +12,11 @@ int main() {
     User user1 ("Paul", "James", "455589049", "21st Avenue");
     User user2("Anna", "Lawry", "7768357509", "Fulton Street");
     User user3("Larry", "Howard", "3351705576", "Libbey Avenue");
+
+    ofstream PaulJames;
+    PaulJames.open("paulj.txt");
+
+    PaulJames<<"User: Paul James "<<user1.getTelephoneNumber()<<" \n"<<endl;
 
     Bank bank1("American Express", "3334445565", "New York");
     Bank bank2("HSBC Bank", "44 1226 261010", "London, UK");
@@ -22,6 +28,10 @@ int main() {
     bA1.recharge(1000);
     bA2.recharge(3600);
     bA3.recharge(2456.68);
+
+    PaulJames<<"Balance of "<<user1.getName()<<" "<<user1.getSurname()<<" = "<<bA1.getBalance()<<endl;
+    PaulJames.close();
+
 
     bA2.transazione(true, bA1, 500);
     bA2.info();
