9c97e4f40e2584651d430ec97569b05e6d76f074
niccolentini <87321791+niccolentini@users.noreply.github.com>
2021-07-17 17:27:47 +0200

    inserimento dati corretti nel main e unit testing

--- test/UserTest.cpp
+++ test/UserTest.cpp
@@ -8,3 +8,11 @@ TEST(User, noAccountStart){
     User u("user","test","12345","test");
     ASSERT_EQ(0, u.bankaccount.size());
 }
+
+TEST(User, getters){
+    User u("user","test","12345","test");
+    ASSERT_EQ("user", u.getName());
+    ASSERT_EQ("test", u.getSurname());
+    ASSERT_EQ("12345", u.getTelephoneNumber());
+    ASSERT_EQ("test", u.getAddress());
+}
